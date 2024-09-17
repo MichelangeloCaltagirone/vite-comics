d51ca20aaee73b2f7931abc012d774588cab9480
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-09-17 17:44:31 +0200

    Done the props of 2 components: AppMain.vue(father) and ComicsCard.vue (child)

--- src/components/ComicsCard.vue
+++ src/components/ComicsCard.vue
@@ -1,8 +1,33 @@
 <script>
+    export default {
+        data() {
+            return {
+            }
+        },
+        props: {
+            picUrl: {
+                type: String,
+                required: true
+            },
+            price: {
+                type: String,
+                required: false
+            },
+            title: {
+                type: String,
+                required: true
+            },
+            bookType: {
+                type: String,
+                required: false
+            }
+        }
+
+    }
 </script>
 
 <template>
-    <h1>Card</h1>
+    <h1>{{ title }}</h1>
 </template>
 
 <style scoped>
