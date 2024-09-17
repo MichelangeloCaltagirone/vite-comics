c8e9f33154d74d38c6f59adf9fcb238df41e83f8
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-09-17 15:37:53 +0200

    Almost finished styling the AppFooterBanner component.

--- src/components/AppFooterBanner.vue
+++ src/components/AppFooterBanner.vue
@@ -2,17 +2,61 @@
 </script>
 
 <template>
-    <div class="container">
+    <div class="bg">
+        <div class="container">
 
-        <h1>footer banner</h1>
+            <div>
+                <button id="btnSingUpNow"><a href="#">SIGN-UP NOW!</a></button>
+                <div class="footerMediaGroup">
+                    <a id="linkFollowUs" href="#">FOLLOW US</a>
+                    <span>ciao</span>
+                    <span>ciao</span>
+                    <span>ciao</span>
+                    <span>ciao</span>
+                    <span>ciao</span>
+                    <span>ciao</span>
+                </div>
+            </div>
 
-    </div>
+        </div>
+    </div>    
 </template>
 
 <style scoped>
+a {
+    text-decoration: none;
+}
+.bg {
+    background-color: #303030;
+    position: relative;
+    z-index: 1;
+}
 .container {
     max-width: 1200px;
     margin: 0 auto;
+    padding: 1rem 0;
+    
+    & div:first-child {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+}
+.footerMediaGroup {
+    display: flex;
+    gap: 10px;
+}
+#btnSingUpNow {
+    padding: 10px 15px;
+    background-color: transparent;
+    border: 1px solid aqua;
+    a {
+        color: white;
+    }
+}
+#linkFollowUs {
+    color: aqua;
+    font-weight: bold;
 }
 
 </style>
